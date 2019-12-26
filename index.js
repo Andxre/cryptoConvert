69ef2abb3403b6a15cf8e57a3b5eabc210f89c53
Andre Dasalla <apd6710@gmail.com>
2019-12-26 18:49:58 +1000

    Inital Javascript testing

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("This is a test")
+
+// Initiate default values to input boxes
+document.getElementById("coin-input").value = "1";
+document.getElementById("currency-input").value = "7,100";
+
+/*
+use Jquery and AJAX to update values using coindesk API
+*/
+
+
+
